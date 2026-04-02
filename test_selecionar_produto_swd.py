2ea484a2f9f82319a243b4b4609fbe01b85b70b1
aamorim1705-a11y <aamorim1705@gmail.com>
2026-04-02 16:25:17 +0100

    Verificações da página de inventário

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -18,5 +18,14 @@ class Teste_Produtos():
 
     def test_selecionar_produto(self):          # metodo de teste
         self.driver.get(self.url)               # abre o navegador
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")  # escreve no campo user-name
-        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")  # escreve senha
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")               # escreve no campo user-name
+        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")               # escreve senha
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click ()  # clique no botao de login
+
+        # transiçao de página
+        assert self.driver.find_element(By.CSS_SELECTOR,"span.title").text == "Products"   # confirma se está escrito Products no elemento
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"  # confirma se é a mochila
+        # confirma preço da mochila
+        assert self.driver.find_element(By. CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == (
+            "$29.99" 
+        )
